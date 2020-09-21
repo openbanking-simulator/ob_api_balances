0734a289a970871f8fd8fcc22de4b865fa3478eb
Madan Narra <narra.madan@outlook.com>
2020-09-22 07:38:07 +1000

    Initial commit

---
+++ src/main/java/com/openbanking/simulator/obapibalances/ObApiBalancesApplication.java
@@ -0,0 +1,13 @@
+package com.openbanking.simulator.obapibalances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObApiBalancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObApiBalancesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openbanking/simulator/obapibalances/ObApiBalancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openbanking.simulator.obapibalances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObApiBalancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
